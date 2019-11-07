c39761b1eb06301d778140ccad94e73d03b93835
Matej Parobek <xparob00@stud.fit.vutbr.cz>
2019-11-07 22:34:34 +0100

    Created models for user repository.

---
+++ Models/UserLoginModel.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Models;
+
+
+class UserLoginModel
+{
+    public $id;
+    public $login;
+    public $password;
+}
---
+++ Models/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Models;
+
+
+class UserModel
+{
+    public $id;
+    public $name;
+    public $surname;
+    public $login;
+    public $password;
+    public $email;
+    public $registration;
+    public $bank_account;
+    public $address;
+}
