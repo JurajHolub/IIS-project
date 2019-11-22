03f120e35d45a7a399f9d40971de30305e571d5c
Matej Parobek <xparob00@stud.fit.vutbr.cz>
2019-11-22 15:49:53 +0100

    Sort added.

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -9,8 +9,40 @@ class TicketController extends Controller
 {
     public function index()
     {
-        $tickets = Ticket::all();
+        $request = request()->validate([
+            'sort' => ['string']
+        ]);
 
-        return view('ticket.index', compact('tickets'));
+        if (!isset($request["sort"]) || $request["sort"] === "Recently updated")
+        {
+            $tickets = Ticket::orderBy('updated_at', 'desc')->get();
+            $sort = "recently_updated";
+        }
+        elseif($request["sort"] === "Oldest") {
+            $tickets = Ticket::orderBy('created_at', 'asc')->get();
+            $sort = "oldest";
+        }
+        elseif ($request["sort"] === "Newest") {
+            $tickets = Ticket::orderBy('created_at', 'desc')->get();
+            $sort = "newest";
+        }
+        elseif ($request["sort"] === "Most commented")
+        {
+            $tickets = Ticket::orderBy('updated_at', 'desc')->get();
+            $sort = "recently_updated";
+        }
+        elseif ($request["sort"] === "Least Commented")
+        {
+            $tickets = Ticket::orderBy('updated_at', 'desc')->get();
+            $sort = "recently_updated";
+        }
+        else
+        {
+            $tickets = Ticket::orderBy('updated_at', 'desc')->get();
+            $sort = "recently_updated";
+        }
+
+
+        return view('ticket.index', compact('tickets','sort'));
     }
 }
--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -3,8 +3,26 @@
 @section ('title', 'Tickets')
 
 @section('content')
-    <div class="container">
+    <div class="container mb-3">
+        <form method="GET" action="">
+            <div class="row">
+                <div class="input-group col-md-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Sort:</span>
+                </div>
+                <select class="form-control" id="sel1" name="sort" onchange="this.form.submit()">
+                    <option @if ( $sort == "newest" ) selected @endif>Newest</option>
+                    <option @if ( $sort == "oldest" ) selected @endif>Oldest</option>
+                    <option @if ( $sort == "recently_updated" ) selected @endif>Recently updated</option>
+                    <option @if ( $sort == "most_commented" ) selected @endif>Most commented</option>
+                    <option @if ( $sort == "least_commented" ) selected @endif>Least commented</option>
+                </select>
+            </div>
+            </div>
+        </form>
+    </div>
 
+    <div class="container">
         <div class="list-group">
             @forelse($tickets as $ticket)
                 <div class="list-group-item my-link" id="ticket-{{ $ticket->id }}">
