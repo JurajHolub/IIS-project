e75a30c0f13c1f5e502fafa41c29df9de2ef3de8
Matej Parobek <xparob00@stud.fit.vutbr.cz>
2019-11-22 20:32:22 +0100

    Bootstrap tuning.

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -3,38 +3,45 @@
 @section ('title', 'Tickets')
 
 @section('content')
-    <div class="container col-md-9">
+    <div class="container">
         <div class="row mb-3">
-        <form class="col-md-3" method="GET" action="">
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">Sort:</span>
-                </div>
-                <select class="form-control" id="sel1" name="sort" onchange="this.form.submit()">
-                    <option @if ( $sort == "newest" ) selected @endif>Newest</option>
-                    <option @if ( $sort == "oldest" ) selected @endif>Oldest</option>
-                    <option @if ( $sort == "recently_updated" ) selected @endif>Recently updated</option>
-                    <option @if ( $sort == "most_commented" ) selected @endif>Most commented</option>
-                    <option @if ( $sort == "least_commented" ) selected @endif>Least commented</option>
-                </select>
+            <div class="col-lg-3 mb-3 mb-lg-0">
+                <form method="GET" action="">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Sort:</span>
+                        </div>
+                        <select class="form-control" id="sel1" name="sort" onchange="this.form.submit()">
+                            <option @if ( $sort == "newest" ) selected @endif>Newest</option>
+                            <option @if ( $sort == "oldest" ) selected @endif>Oldest</option>
+                            <option @if ( $sort == "recently_updated" ) selected @endif>Recently updated</option>
+                            <option @if ( $sort == "most_commented" ) selected @endif>Most commented</option>
+                            <option @if ( $sort == "least_commented" ) selected @endif>Least commented</option>
+                        </select>
+                    </div>
+                </form>
             </div>
-        </form>
-        <a class="btn btn-success ml-auto mr-3" href="tickets/create">New issue</a>
+            <div class="col-lg-2 offset-lg-7">
+                <a class="btn btn-success btn-block" href="tickets/create">New issue</a>
             </div>
-
-        <div class="list-group">
-            @forelse($tickets as $ticket)
-                <div class="list-group-item my-link" id="ticket-{{ $ticket->id }}">
-                    <strong><a href="tickets/{{ $ticket->id }}">{{ $ticket->title }}</a></strong>
-                    <p class="info mb-0 mt-3 p-0">
-                        Opened by <a href="#">{{ $ticket->author->login }}</a>
-                        {{ $ticket->updated_at->diffForHumans() }}
-                        <span class="pl-3"><i class="fa fa-comments"></i> <a href="#">14 comments</a></span>
-                    </p>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="list-group">
+                    @forelse($tickets as $ticket)
+                        <div class="list-group-item my-link" id="ticket-{{ $ticket->id }}">
+                            <strong><a href="tickets/{{ $ticket->id }}">{{ $ticket->title }}</a></strong>
+                            <p class="info mb-0 mt-3 p-0">
+                                Opened by <a href="#">{{ $ticket->author->login }}</a>
+                                {{ $ticket->updated_at->diffForHumans() }}
+                                <span class="pl-3"><i class="fa fa-comments"></i> <a href="#">14 comments</a></span>
+                            </p>
+                        </div>
+                    @empty
+                        <p>No tickets</p>
+                    @endforelse
                 </div>
-            @empty
-                <p>No tickets</p>
-            @endforelse
+            </div>
         </div>
     </div>
 @endsection
