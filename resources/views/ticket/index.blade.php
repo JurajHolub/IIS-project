01a0a71f0c0e4a0c001a9da0894950e12e544a1a
Matej Parobek <xparob00@stud.fit.vutbr.cz>
2019-11-23 18:12:04 +0100

    Nekaz mi moju stranku, mne sa to tak paci.

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -4,25 +4,28 @@
 
 @section('content')
     <div class="container">
-        <div class="col-lg-3 mb-3 mb-lg-0">
-            <form method="GET" action="">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">Sort:</span>
+        <div class="row mb-3">
+            <div class="col-lg-3 mb-3 mb-lg-0">
+                <form method="GET" action="">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Sort:</span>
+                        </div>
+                        <select class="form-control" id="sel1" name="sort" onchange="this.form.submit()">
+                            <option @if ( $sort == "newest" ) selected @endif>Newest</option>
+                            <option @if ( $sort == "oldest" ) selected @endif>Oldest</option>
+                            <option @if ( $sort == "recently_updated" ) selected @endif>Recently updated</option>
+                            <option @if ( $sort == "most_commented" ) selected @endif>Most commented</option>
+                            <option @if ( $sort == "least_commented" ) selected @endif>Least commented</option>
+                        </select>
                     </div>
-                    <select class="form-control" id="sel1" name="sort" onchange="this.form.submit()">
-                        <option @if ( $sort == "newest" ) selected @endif>Newest</option>
-                        <option @if ( $sort == "oldest" ) selected @endif>Oldest</option>
-                        <option @if ( $sort == "recently_updated" ) selected @endif>Recently updated</option>
-                        <option @if ( $sort == "most_commented" ) selected @endif>Most commented</option>
-                        <option @if ( $sort == "least_commented" ) selected @endif>Least commented</option>
-                    </select>
-                </div>
-            </form>
+                </form>
+            </div>
+            <div class="col-lg-2 offset-lg-7">
+                <a class="btn btn-success btn-block" href="/tickets/create">New issue</a>
+            </div>
         </div>
-        <div class="col-lg-2 p-3">
-            <a class="btn btn-success btn-block" href="/tickets/create">New issue</a>
-        </div>
+
         <div class="row">
             <div class="col-12">
                 <div class="list-group">
