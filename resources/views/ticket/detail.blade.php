e37be4114b431127c0827f59e3293a88c9da465b
JurajHolub <xholub40@stud.fit.vutbr.cz>
2019-11-23 16:27:26 +0100

    HTML template.

--- resources/views/ticket/detail.blade.php
+++ resources/views/ticket/detail.blade.php
@@ -16,6 +16,13 @@
         <br>
         <h3>Comments</h3>
         <div class="list-group">
+            @foreach ($comments as $comment)
+                <a class="list-group-item">
+                    <h4 class="list-group-item-heading">{{ $comment->id }}</h4>
+                    <h6 class="list-group-item-heading">{{ $comment->created_at }}</h6>
+                    <p class="list-group-item-text"> {{ $comment->description }}</p>
+                </a>
+            @endforeach
             <a class="list-group-item">
                 <h4 class="list-group-item-heading">jurko</h4>
                 <h6 class="list-group-item-heading">1.1.2019 - 09:28</h6>
@@ -37,6 +44,12 @@
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                 </p>
             </a>
+        </div>
+        <div class="border pt-3">
+            <form>
+                <textarea class="form-control" rows="2" id="comment" name="comment" placeholder="Write comment..."></textarea>
+                <button type="submit" id="submit" class="btn btn-success">Post</button>
+            </form>
         </div>
     </div>
 @endsection
